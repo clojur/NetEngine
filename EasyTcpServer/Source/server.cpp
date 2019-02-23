a2ff83c88979bfb4a1d0c404bd8b8d6c7e7fde69
JiangSi <1326050796@qq.com>
2019-02-23 14:46:42 +0800

    服务端跨平台；

--- EasyTcpServer/Source/server.cpp
+++ EasyTcpServer/Source/server.cpp
@@ -1,10 +1,22 @@
-﻿#define WIN32_LEAN_AND_MEAN
+﻿#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
+#define _CRT_SECURE_NO_WARNINGS
 #include<windows.h>
 #include<WinSock2.h>
-#include<vector>
-#include<stdio.h>
+#else
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+#define SOCKET     int
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+#endif // _WIN32
+
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
 
 enum CMD
 {
@@ -87,11 +99,11 @@ int HandleData(SOCKET clientSocket)
 	//缓冲区
 	char szRecv[4096] = {};
 	//5 接受客户端请求数据
-	int nLen = recv(clientSocket, szRecv, sizeof(DataHeader), 0);
+	int nLen = (int)recv(clientSocket, szRecv, sizeof(DataHeader), 0);
 	DataHeader* header = (DataHeader*)szRecv;
 	if (nLen <= 0)
 	{
-		printf("客户端<Socket=%d>退出，任务结束。\n",(int)clientSocket);
+		printf("客户端<Socket=%d>退出，任务结束。\n", (int)clientSocket);
 		return -1;
 	}
 
@@ -101,7 +113,7 @@ int HandleData(SOCKET clientSocket)
 	{
 		recv(clientSocket, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
 		Login* login = (Login*)szRecv;
-		printf("收到客户端<Socket=%d>请求：CMD_LOGIN 数据长度：%d userName=%s passWord=%s\n", (int)clientSocket,login->dataLength, login->userName, login->passWord);
+		printf("收到客户端<Socket=%d>请求：CMD_LOGIN 数据长度：%d userName=%s passWord=%s\n", (int)clientSocket, login->dataLength, login->userName, login->passWord);
 		LoginResult ret;
 		send(clientSocket, (char*)&ret, sizeof(ret), 0);
 	}
@@ -129,9 +141,11 @@ int HandleData(SOCKET clientSocket)
 
 int main()
 {
+#ifdef _WIN32
 	WORD version = MAKEWORD(2, 2);
 	WSADATA data;
 	WSAStartup(version, &data);
+#endif
 	///服务端建立
 	//1 建立一个socket
 	SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -147,7 +161,12 @@ int main()
 	sockaddr_in _sin = {};
 	_sin.sin_family = AF_INET;
 	_sin.sin_port = htons(4567);
-	_sin.sin_addr.S_un.S_addr = INADDR_ANY;//inet_addr("127.0.0.1");
+
+#ifdef _WIN32
+	_sin.sin_addr.S_un.S_addr = INADDR_ANY;
+#else
+	_sin.sin_addr.s_addr = INADDR_ANY;
+#endif // _WIN32
 	if (bind(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in)) == SOCKET_ERROR)
 	{
 		printf("错误,绑定网络端口失败！\n");
@@ -178,16 +197,21 @@ int main()
 		FD_SET(_sock, &fdWrite);
 		FD_SET(_sock, &fdExcept);
 
+		SOCKET maxSock = _sock;
+
 		size_t clientCount = g_clients.size();
 		for (size_t n = 0; n <clientCount; ++n)
 		{
 			FD_SET(g_clients[n], &fdRead);
+			//查找最大描述符
+			if (maxSock<g_clients[n])
+				maxSock = g_clients[n];
 		}
 
 		///nfds 是一个整数值，是指fd_set集合中所有的SOCKET的范围，而不是数量
 		///即是所有文件描述符（sock）最大值+1 在windows中这个参数可以写0
-		timeval tv = {1,0};
-		int ret= select((int)_sock + 1, &fdRead, &fdWrite, &fdExcept, &tv);
+		timeval tv = { 1,0 };
+		int ret = select(maxSock + 1, &fdRead, &fdWrite, &fdExcept, &tv);
 
 		if (ret < 0)
 		{
@@ -201,7 +225,12 @@ int main()
 			sockaddr_in _clinetAddr = {};
 			int nAddrLen = sizeof(sockaddr_in);
 			SOCKET _clientSock = INVALID_SOCKET;
+#ifdef _WIN32
 			_clientSock = accept(_sock, (sockaddr*)&_clinetAddr, &nAddrLen);
+#else
+			_clientSock = accept(_sock, (sockaddr*)&_clinetAddr, (socklen_t*)&nAddrLen);
+#endif
+
 			if (INVALID_SOCKET == _clientSock)
 			{
 				printf("错误，接收到无效客户端SOCKET...\n");
@@ -210,19 +239,24 @@ int main()
 			for (size_t n = 0; n <clientCount; ++n)
 			{
 				NewUserJoin userJoin;
-				send(g_clients[n],(const char*)&userJoin,sizeof(NewUserJoin),0);
+				send(g_clients[n], (const char*)&userJoin, sizeof(NewUserJoin), 0);
 			}
 			g_clients.push_back(_clientSock);
 			printf("新客户端加入：SOCK=%d,IP= %s\n", int(_clientSock), inet_ntoa(_clinetAddr.sin_addr));
 		}
-
-		for (size_t n = 0; n < fdRead.fd_count; ++n)
+		size_t count = g_clients.size();
+		for (size_t i = 0; i<count; ++i)
 		{
-			if (-1 == HandleData(fdRead.fd_array[n]))
+			if (FD_ISSET(g_clients[i], &fdRead))
 			{
-				auto iter = find(g_clients.begin(), g_clients.end(), fdRead.fd_array[n]);
-				if (iter != g_clients.end())
-					g_clients.erase(iter);
+				if (-1 == HandleData(g_clients[i]))
+				{
+					auto iter = std::find(g_clients.begin(), g_clients.end(), g_clients[i]);
+					if (iter != g_clients.end())
+					{
+						g_clients.erase(iter);
+					}
+				}
 			}
 		}
 
@@ -230,6 +264,7 @@ int main()
 		//printf("空闲时间处理其他业务...\n");
 	}
 
+#ifdef _WIN32
 	for (size_t n = 0; n < g_clients.size(); ++n)
 	{
 		closesocket(g_clients[n]);
@@ -237,8 +272,15 @@ int main()
 	//6 关闭套接字
 	closesocket(_sock);
 	///
-	printf("已退出，任务结束。");
 	WSACleanup();
+#else
+	for (size_t n = 0; n < g_clients.size(); ++n)
+	{
+		close(g_clients[n]);
+	}
+	close(_sock);
+#endif
+	printf("已退出，任务结束。");
 	getchar();
 
 	return 0;
