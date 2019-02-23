3886340700cdfb55a4e74e9ad402dec05aa36daf
JiangSi <1326050796@qq.com>
2019-02-23 12:57:06 +0800

    字符统一改为UTF-8

--- EasyTcpClient/Source/client.cpp
+++ EasyTcpClient/Source/client.cpp
@@ -1,4 +1,4 @@
-#ifdef _WIN32
+﻿#ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
@@ -186,13 +186,16 @@ int main()
 		printf("客户端建立套接字成功！\n");
 	}
 	//2 连接服务器
+	char buf[256] = {};
+	printf("请输入服务器IP:");
+	scanf("%s",buf);
 	sockaddr_in _sin = {};
 	_sin.sin_family = AF_INET;
 	_sin.sin_port = htons(4567);
 #ifdef _WIN32
-	_sin.sin_addr.S_un.S_addr = inet_addr("192.168.217.128");
+	_sin.sin_addr.S_un.S_addr = inet_addr(buf);
 #else
-	_sin.sin_addr.s_addr = inet_addr("112.163.107.19");
+	_sin.sin_addr.s_addr = inet_addr(buf);
 #endif // _WIN32
 	int rec = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
 	if (SOCKET_ERROR == rec)
